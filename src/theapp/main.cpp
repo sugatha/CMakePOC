fcb8315f879047e81026295dd45f40ec5a3a1eb0
Sugathan.anandan <sugathan.anadan@tallysolutions.com>
2019-11-22 10:37:15 +0530

    working model of cmake

---
+++ src/theapp/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "mylibshared.h"
+
+int main()
+{
+   std::cout << "Hey! - a shout from a terra-firmian :) " << std::endl;
+
+   UseMyClass myclass_obj;
+   
+   myclass_obj.PrintUseMyClassHello();
+   return 0;
+}
